16c92cc1116f8e39b4e298b8d21ffbb8757fd925
oikomi <miaohonghit@gmail.com>
2015-12-08 10:46:19 +0800

    init

--- src/main/java/com/baidu/uaq/imgoptdaemon/core/TaskRun.java
+++ src/main/java/com/baidu/uaq/imgoptdaemon/core/TaskRun.java
@@ -52,7 +52,7 @@ public class TaskRun {
 
         while(true) {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
