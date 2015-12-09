77c3979c0f22fbe8f06eaf1ec8843f377152d94b
oikomi <miaohonghit@gmail.com>
2015-12-09 14:13:31 +0800

    init

--- src/main/java/com/baidu/uaq/imgoptdaemon/server/controller/ImgOptDaemonAction.java
+++ src/main/java/com/baidu/uaq/imgoptdaemon/server/controller/ImgOptDaemonAction.java
@@ -5,6 +5,9 @@ import com.baidu.uaq.imgoptdaemon.config.Config;
 import com.baidu.uaq.imgoptdaemon.config.Const;
 import com.baidu.uaq.imgoptdaemon.core.TaskRunV2;
 import com.baidu.uaq.imgoptdaemon.db.Redis;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,15 @@ import java.io.File;
 @RequestMapping("/uaq/v1")
 public class ImgOptDaemonAction {
     private Config config = Config.getInstance();
+
+    @PostConstruct
+    public void init() {
+        File f = new File(Const.DOWNLOAD_IMG_BASE_PATH);
+        f.mkdirs();
+        f = new File(Const.OPT_IMG_BASE_PATH);
+        f.mkdirs();
+    }
+
 
 //    @PostConstruct
 //    public void init() {
@@ -59,4 +71,53 @@ public class ImgOptDaemonAction {
 //
 //        return respCmd;
 //    }
+
+
+    @RequestMapping(value = "/imgopt", method = RequestMethod.GET)
+    public @ResponseBody RespCmd imgoptMonitor() {
+        RespCmd respCmd = new RespCmd();
+
+        respCmd.setCode(0);
+        respCmd.setInfo("success");
+
+        return respCmd;
+    }
+
+    @RequestMapping(value = "/imgopt", method = RequestMethod.POST)
+    public @ResponseBody RespCmd imgopt(@RequestBody String reqBody) {
+        RespCmd respCmd = new RespCmd();
+        if (reqBody == null) {
+            respCmd.setCode(-1);
+            respCmd.setInfo("error");
+
+            return respCmd;
+        }
+
+        Redis redis = new Redis(config.getRedisAddr(), config.getRedisPort());
+        System.out.println("store in redis");
+        redis.pushTask(reqBody);
+        System.out.println("store in redis  end");
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = null;
+//        try {
+//            HttpGet httpget = new HttpGet("http://127.0.0.1:8020/uaq/v1/doimgopt");
+//            response = httpclient.execute(httpget);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (response != null) {
+//                try {
+//                    response.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+
+        respCmd.setCode(0);
+        respCmd.setInfo("success");
+
+        return respCmd;
+    }
 }
